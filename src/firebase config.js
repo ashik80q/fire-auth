812759890b52f9835e84e8e790556955958a782a
Ashik khandakar <ashikbpdb@gmail.com>
2020-04-14 20:04:47 +0600

    firebase auth app

---
+++ src/firebase config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbjIXgUlS6U735EPfjkSP3dZXHQCto2qE",
+    authDomain: "ema-john-simple-6f294.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-6f294.firebaseio.com",
+    projectId: "ema-john-simple-6f294",
+    storageBucket: "ema-john-simple-6f294.appspot.com",
+    messagingSenderId: "290913252957",
+    appId: "1:290913252957:web:d9e9f795552dd5005cffb4"
+  };
+  export default firebaseConfig;
